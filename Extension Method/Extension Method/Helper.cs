fa49941c890ccc3333b851c2b00867128a9349b1
sunkeys1 <87263638+sunkeys1@users.noreply.github.com>
2021-07-24 18:31:50 +0700

    Add files via upload

---
+++ Extension Method/Extension Method/Helper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension_Method
+{
+    public static class Helper
+    {
+        public static bool IsEven(this int i)
+        {
+            return i % 2 == 0;
+        }
+        public static bool IsDevidedBy (this int i, int j)
+        {
+            return i % j == 0;
+
+        }
+        public static string ConverterToString(this IEnumerable collection)
+        {
+            var result = "";
+            foreach(var item in collection)
+            {
+                result += item.ToString() + " \r\n";
+                
+            }
+            return result;
+        }
+
+        public static Road CreateRandomRoad(this Road road, int min, int max)
+        {
+            
+            var rnd = new Random(Guid.NewGuid().ToByteArray().Sum(x => x));
+            road.Number = "М" + rnd.Next(1, 100);
+            road.Lenght = rnd.Next(min, max);
+            return road;
+
+        }
+    }
+}
