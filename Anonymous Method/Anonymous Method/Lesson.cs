8be280783dc8dd39c8df811edb483d144cd4c309
sunkeys1 <basatax5@gmail.com>
2021-07-30 21:35:05 +0700

    add

---
+++ Anonymous Method/Anonymous Method/Lesson.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anonymous_Method
+{
+    class Lesson
+    {
+        public string Name { get; }
+        public DateTime Begin { get; private set; }
+
+        public event EventHandler<DateTime> Started;
+        public Lesson(string name)
+        {
+
+            Name = name;
+        }
+        public void Start()
+        {
+            Begin = DateTime.Now;
+            Started?.Invoke(this, Begin);
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+
+    }
+}
---
+++ Anonymous Method/Anonymous Method/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anonymous_Method
+{
+    class Program
+    {
+        delegate int MyHandler(int i);
+        static void Main(string[] args)
+        {
+
+            var lesson = new Lesson("Начало");
+            lesson.Started += (sender, date) =>
+            {
+                Console.WriteLine(sender);
+                Console.WriteLine(date);
+            };
+
+            lesson.Start();
+
+
+
+
+
+
+
+
+
+
+
+            if (int.TryParse(Console.ReadLine(), out int result))
+            {
+                MyHandler handler = delegate (int i)
+                {
+                    var r = i * i;
+                    Console.WriteLine(r);
+                    return r;
+                };
+                handler += Method;
+                handler(result);
+                handler(30);
+                
+            }
+
+
+            MyHandler lambdahandler = (i) => // i * i;
+            {
+
+                var r = i * i;
+                Console.WriteLine(r);
+                return r;
+            };
+            lambdahandler(56);
+
+
+            Console.ReadLine();
+            
+        }
+
+        public static int Method(int i)
+        {
+            var r = i * i * i;
+            Console.WriteLine(r);
+            return r;
+        }
+    }
+}
