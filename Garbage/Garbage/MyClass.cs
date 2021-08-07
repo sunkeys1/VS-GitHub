52d8667ddad40125cfd896977df4ce3f0f702d4d
sunkeys1 <basatax5@gmail.com>
2021-08-07 17:07:03 +0700

    gaba

---
+++ Garbage/Garbage/MyClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garbage
+{
+    class MyClass : IDisposable
+    {
+        public MyClass() {} // констркутор
+
+        ~MyClass() { } //деструктор
+
+        public void Dispose()
+        {
+            GC.Collect();
+        }
+    }
+}
---
+++ Garbage/Garbage/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garbage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //GC.Collect(0 /*очистка 0 поколения*/);
+            Console.WriteLine(GC.GetTotalMemory(false));
+            for(var i = 0; i < 10000; i++)
+            {
+
+                var obj = (object)i;
+                int j = (int)obj;
+            }
+            Console.WriteLine(GC.GetTotalMemory(false));
+            using (var c = new MyClass())
+            {
+
+
+            }
+
+
+            Console.WriteLine(GC.GetTotalMemory(false));
+            GC.Collect();
+            Console.WriteLine(GC.GetTotalMemory(false));
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
