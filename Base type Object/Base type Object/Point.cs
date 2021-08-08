4284a1dfe7e7377df6194883d2ae17afaea23f4d
sunkeys1 <basatax5@gmail.com>
2021-08-08 18:40:31 +0700

    add

---
+++ Base type Object/Base type Object/Point.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base_type_Object
+{
+    class Point
+    {
+        public int X { get; set; }
+
+        public override bool Equals(object obj) // если вылезает null это не должен быть exeption 
+        {                                       // и нужно вернуть false
+            //if(obj == null)
+            //{
+            //    return false;
+            //}
+
+            //var point = obj as Point;  всю вот эту конструкцию можно заменить на: (29)
+            //if(point != null)
+            //{
+
+            //}
+            //else
+            //{
+            //    return false;
+            //}
+            if(obj is Point point)
+            {
+                return point.X == X;
+            }
+            else
+            {
+                return false;
+            }
+            
+
+        }
+        public override int GetHashCode()
+        {
+            return X;
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+    }
+}
---
+++ Base type Object/Base type Object/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base_type_Object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // System.Object === obj
+            // System.Int32 === int
+            object obj = new object();
+            var i = 5;
+            var j = 5;
+            Console.WriteLine(i.Equals(j));
+            object oi = (object)i;  // var oi = (object)i;
+            object oj = (object)j;
+            Console.WriteLine(oi.Equals(oj));
+            var p1 = new Point() { X = 5 }; 
+            var p2 = new Point() { X = 5 };   // нужно убрать в Point.cs с 29 строки проверку типов.
+            Console.WriteLine(p1.Equals(p2)); // тут будет false тк объекты находятся в разных участках памяти
+            Console.WriteLine();
+            Console.WriteLine(i.GetHashCode());
+            Console.WriteLine(oj.GetHashCode());
+            Console.WriteLine(new MyClass().GetHashCode());
+            Console.WriteLine(p1.GetHashCode());
+            Console.WriteLine(i.ToString());
+            Console.WriteLine(p1.ToString());
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
