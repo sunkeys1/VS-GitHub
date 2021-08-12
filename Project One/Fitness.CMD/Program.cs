aa3d8c03276deeee46176e424c7129a6abbca045
sunkeys1 <basatax5@gmail.com>
2021-08-12 20:49:47 +0700

    added reading existed user and add not existed user

--- Project One/Fitness.CMD/Program.cs
+++ Project One/Fitness.CMD/Program.cs
@@ -13,7 +13,6 @@ namespace Fitness.CMD
         static void Main(string[] args)
         {
             Console.WriteLine("Здравствуйте!");
-
             Console.WriteLine("Введите имя пользователя: ");
             var name = Console.ReadLine();
 
@@ -23,36 +22,43 @@ namespace Fitness.CMD
             {
                 Console.Write("Введите пол: ");
                 var gender = Console.ReadLine();
+                var birthDate = ParseDateTime();
+                var weight = ParseDouble("вес");
+                var height = ParseDouble("рост");
 
-                DateTime birthDate;
-                double weight;
-                double height;
-                while (true)
-                {
-                    Console.Write("Введите дату рождения (dd.MM.yyyy): ");
-                    if (DateTime.TryParse(Console.ReadLine(), out birthDate))
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Неверный формат даты рождения.");
-                    }
-                }
-                userController
+                userController.SetNewUserData(gender, birthDate, weight, height);
             }
             Console.WriteLine(userController.CurrentUser);
             Console.ReadLine();
         }
-        private static T ParseDouble<T>(string name)
+
+        private static DateTime ParseDateTime()
+        {
+            DateTime birthDate;
+            while (true)
+            {
+                Console.Write("Введите дату рождения (dd.MM.yyyy): ");
+                if (DateTime.TryParse(Console.ReadLine(), out birthDate))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Неверный формат даты рождения.");
+                }
+            }
+
+            return birthDate;
+        }
+
+        private static double ParseDouble(string name)
         {
             while (true)
             {
                 Console.Write($"Введите {name}: ");
-                if (double.TryParse(Console.ReadLine(), out T value))
+                if (double.TryParse(Console.ReadLine(), out double value))
                 {
-                    return value
-                    break;
+                    return value; 
                 }
                 else
                 {
