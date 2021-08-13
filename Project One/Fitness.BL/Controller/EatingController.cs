d6bf8eb4c964b64e48f400c8c51474c9a7920892
sunkeys1 <basatax5@gmail.com>
2021-08-13 19:33:37 +0700

    rework save load, add some

--- Project One/Fitness.BL/Controller/EatingController.cs
+++ Project One/Fitness.BL/Controller/EatingController.cs
@@ -9,31 +9,60 @@ using System.Threading.Tasks;
 
 namespace Fitness.BL.Controller
 {
-    public class EatingController
+    public class EatingController : ControllerBase
     {
+        private const string FOODS_FILE_NAME = "foods.dat";
+        private const string EATINGS_FILE_NAME = "eatings.dat";
         private readonly User user;
         public List<Food> Foods { get; }
+        public Eating Eating { get; }
         public EatingController(User user)
         {
             this.user = user ?? throw new ArgumentNullException("Пользователь не может быть пустым.", nameof(user));
             Foods = GetAllFoods();
+            Eating = GetEating();
+        }
+        public bool Add(string foodName, double weight)
+        {
+           var food = Foods.SingleOrDefault(f => f.Name == foodName);
+            if(food != null)
+            {
+                Eating.Add(food, weight);
+                Save();
+                return true;
+            }
+            return false;
+        }
+        public void Add(Food food, double weight)
+        {
+            var product = Foods.SingleOrDefault(f => f.Name == food.Name);
+            if(product == null)
+            {
+                Foods.Add(food);
+                Eating.Add(food, weight);
+                Save();
+                
+            }
+            else
+            {
+                Eating.Add(product, weight);
+                Save();
+            }
+        }
+
+        private Eating GetEating()
+        {
+            return Load<Eating>(EATINGS_FILE_NAME) ?? new Eating(user);
         }
 
         private List<Food> GetAllFoods()
         {
-            var formatter = new BinaryFormatter();
-            using (var fs = new FileStream("foods.dat", FileMode.OpenOrCreate))
-            {
-                if (fs.Length > 0 && formatter.Deserialize(fs) is List<Food> foods)
-                {
-                    return foods;
-
-                }
-                else
-                {
-                    return new List<Food>();
-                }
-            }
+            return Load<List<Food>>(FOODS_FILE_NAME) ?? new List<Food>();
+        }
+        private void Save()
+        {
+            Save(FOODS_FILE_NAME, Foods);
+            Save(EATINGS_FILE_NAME, Eating);
         }
     }
 }
--- Project One/Fitness.BL/Controller/UserController.cs
+++ Project One/Fitness.BL/Controller/UserController.cs
@@ -12,8 +12,9 @@ namespace Fitness.BL.Controller
     /// <summary>
     /// Контроллер пользователя.
     /// </summary>
-    public class UserController
+    public class UserController : ControllerBase
     {
+        private const string USERS_FILE_NAME = "users.dat";
         /// <summary>
         /// Пользователь приложения.
         /// </summary>
@@ -51,20 +52,8 @@ namespace Fitness.BL.Controller
         /// <returns></returns>
         private List<User> GetUsersData()
         {
-            var formatter = new BinaryFormatter();
-            using (var fs = new FileStream("users.dat", FileMode.OpenOrCreate))
-            {
-                if (fs.Length > 0 && formatter.Deserialize(fs) is List<User> users)
-                {
-                    return users;
-
-                }
-                else
-                {
-                    return new List<User>();
-                }
-            }
-            
+            return Load<List<User>>(USERS_FILE_NAME) ?? new List<User>();
+  
         }
 
         public void SetNewUserData(string genderName, DateTime birthDate, double weight = 1, double height = 1)
@@ -82,16 +71,8 @@ namespace Fitness.BL.Controller
         /// </summary>
         public void Save()
         {
-            var formatter = new BinaryFormatter();
-            using (var fs = new FileStream("users.dat", FileMode.OpenOrCreate))
-            {
-                formatter.Serialize(fs, Users);
-            }
+            Save(USERS_FILE_NAME, Users);
         }
-        /// <summary>
-        /// Получить данные пользователя.
-        /// </summary>
-        /// <returns>Пользователь приложения.</returns>
         
     }
 }
