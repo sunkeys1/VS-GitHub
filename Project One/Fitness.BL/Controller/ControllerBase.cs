c0dae9e90dd9ca361b63bf176e8853c1e22db7b1
sunkeys1 <basatax5@gmail.com>
2021-08-17 19:59:08 +0700

    Saver to interface

--- Project One/Fitness.BL/Controller/ControllerBase.cs
+++ Project One/Fitness.BL/Controller/ControllerBase.cs
@@ -10,29 +10,14 @@ namespace Fitness.BL.Controller
 {
     public abstract class ControllerBase
     {
+        protected IDataSaver saver = new SerializeDataSaver();
         protected void Save(string fileName, object item)
         {
-            var formatter = new BinaryFormatter();
-            using (var fs = new FileStream(fileName, FileMode.OpenOrCreate))
-            {
-                formatter.Serialize(fs, item);
-            }
+            saver.Save(fileName, item);
         }
         protected T Load<T>(string fileName)
         {
-            var formatter = new BinaryFormatter();
-            using (var fs = new FileStream(fileName, FileMode.OpenOrCreate))
-            {
-                if (fs.Length > 0 && formatter.Deserialize(fs) is T items)
-                {
-                    return items;
-
-                }
-                else
-                {
-                    return default(T);
-                }
-            }
+            return saver.Load<T>(fileName);
         }
     }
 }
---
+++ Project One/Fitness.BL/Controller/IDataSaver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness.BL.Controller
+{
+    public interface IDataSaver
+    {
+        void Save(string fileName, object item);
+        T Load<T>(string fileName);
+    }
+}
