844706a65444a610041de523cb01649f708eb661
sunkeys1 <basatax5@gmail.com>
2021-08-27 01:23:59 +0700

    peek pop push

---
+++ Stack/Stack/Model/EasyStack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack.Model
+{
+    public class EasyStack<T>
+    {
+        private List<T> items = new List<T>();
+        public int Count => items.Count;
+        public bool IsEmpty => items.Count == 0;
+        public void Push(T item)
+        {
+            items.Add(item);
+        }
+        public T Pop()
+        {
+            if(!IsEmpty)
+            {
+                var item = items.LastOrDefault();
+                items.Remove(item);
+                return item;
+            }
+            else
+            {
+                throw new NullReferenceException("Ало! Стек пуст!");
+            }
+        }
+        public T Peek()
+        {
+            if (!IsEmpty)
+            {
+                return items.LastOrDefault();
+            }
+            else
+            {
+                throw new NullReferenceException("Ало! Стек пуст!");
+            }
+        }
+        public override string ToString()
+        {
+            return $"Стек с {Count} элементами.";
+        }
+    }
+}
---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,25 @@
+﻿using Stack.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var easyStack = new EasyStack<int>();
+            easyStack.Push(1);
+            easyStack.Push(2);
+            easyStack.Push(3);
+            var item = easyStack.Pop();
+            var item2 = easyStack.Peek();
+            Console.WriteLine(item);
+            Console.WriteLine(item2);
+            Console.ReadLine();
+        }
+    }
+}
