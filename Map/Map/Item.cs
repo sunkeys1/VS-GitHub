6f6907858b5fcc05cd06e49ef519036f30186074
sunkeys1 <basatax5@gmail.com>
2021-09-05 16:00:03 +0700

    EasyMap

---
+++ Map/Map/Item.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Map
+{
+    class Item<TKey, TValue>
+    {
+        public TKey Key { get; set; }
+        public TValue Value { get; set; }
+        public Item(TKey key, TValue value)
+        {
+            // проверки!
+            Key = key;
+            Value = value;
+        }
+        public override int GetHashCode()
+        {
+            return Key.GetHashCode();
+        }
+        public override string ToString()
+        {
+            return Value.ToString(); 
+        }
+    }
+}
---
+++ Map/Map/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Map
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var easyMap = new EasyMap<int, string>();
+            easyMap.Add(new Item<int, string>(1, "Один"));
+            easyMap.Add(new Item<int, string>(2, "Два"));
+            easyMap.Add(new Item<int, string>(3, "Три"));
+            easyMap.Add(new Item<int, string>(4, "Четыре"));
+            easyMap.Add(new Item<int, string>(5, "Пять"));
+            foreach(var item in easyMap)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine(easyMap.Search(7) ?? "Нету такого!");
+            Console.WriteLine(easyMap.Search(3) ?? "Нету такого!");
+            easyMap.Remove(3);
+            easyMap.Remove(1);
+            foreach (var item in easyMap)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
