3f3ab820a3f9c76ebce528f770efae6ecf196ad0
sunkeys1 <basatax5@gmail.com>
2021-09-07 17:32:56 +0700

    Created binary tree

---
+++ BinarySearchTree/BinarySearchTree/Node.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Node<T> : IComparable
+        where T : IComparable
+    {
+        public T Data { get; private set; }
+        public Node<T> Left { get; private set; } // тут должны быть проверки на доступ! (лучше полное свойство с проверкой!)
+        public Node<T> Right { get; private set; }
+        public Node(T data)
+        {
+            Data = data;
+        }
+        public Node(T data, Node<T> left, Node<T> right)
+        {
+            Left = left;
+            Right = right;
+            Data = data;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if(obj is Node<T> item)
+            {
+                return Data.CompareTo(item);
+            }
+            else
+            {
+                throw new ArgumentException("Несовпадение типов!");
+            }
+        }
+    }
+}
---
+++ BinarySearchTree/BinarySearchTree/Tree.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Tree<T>
+        where T : IComparable
+    {
+        public Node<T> Root { get; private set; }
+        public int Count { get; private set; }
+        public void Add(T data)
+        {
+
+        }
+    }
+}
