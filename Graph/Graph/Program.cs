2de08ee760429d6ce5a7af18e785c13dde2f758f
sunkeys1 <basatax5@gmail.com>
2021-09-18 14:16:30 +0700

    array matrix

--- Graph/Graph/Program.cs
+++ Graph/Graph/Program.cs
@@ -10,6 +10,51 @@ namespace Graph
     {
         static void Main(string[] args)
         {
+            var graph = new Graph();
+
+            var v1 = new Vertex(1); // а вообще эти данные выгружаются из какого-нибудь XML, JSOM, БД
+            var v2 = new Vertex(2);
+            var v3 = new Vertex(3);
+            var v4 = new Vertex(4);
+            var v5 = new Vertex(5);
+            var v6 = new Vertex(6);
+            var v7 = new Vertex(7);
+            graph.AddVertex(v1);
+            graph.AddVertex(v2);
+            graph.AddVertex(v3);
+            graph.AddVertex(v4);
+            graph.AddVertex(v5);
+            graph.AddVertex(v6);
+            graph.AddVertex(v7);
+
+            graph.AddEdge(v1, v2);
+            graph.AddEdge(v1, v3);
+            graph.AddEdge(v3, v4);
+            graph.AddEdge(v2, v5);
+            graph.AddEdge(v2, v6);
+            graph.AddEdge(v6, v5);
+            graph.AddEdge(v5, v6);
+
+            var matrix = graph.GetMatrix();
+            for (int i = 0; i < graph.VertexCount; i++)
+            {
+                Console.Write(i + 1);
+                for (int j = 0; j < graph.VertexCount; j++)
+                {
+                    Console.Write(" | " + matrix[i, j] + " | "); 
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("_________________________________________________");
+            Console.WriteLine();
+            for (int i = 0; i < graph.VertexCount; i++)
+            {
+                Console.Write($"  | {i + 1} |");
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
--- Graph/Graph/Vertex.cs
+++ Graph/Graph/Vertex.cs
@@ -8,5 +8,15 @@ namespace Graph
 {
     class Vertex
     {
+        public int Number { get; set; }
+        public Vertex(int number)
+        {
+            Number = number;
+        }
+        public override string ToString()
+        {
+            return Number.ToString(); 
+        }
+
     }
 }
