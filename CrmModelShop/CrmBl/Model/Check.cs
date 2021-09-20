904024f3e420c178765bf976615be80d35b2b901
sunkeys1 <basatax5@gmail.com>
2021-09-20 16:03:37 +0700

    new project

---
+++ CrmModelShop/CrmBl/Model/Check.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmBl.Model
+{
+    class Check
+    {
+        public int CheckId { get; set; }
+        public int CustomerId { get; set; }
+        public virtual Customer Customer { get; set; }
+        public int SellerId { get; set; } // это 
+        public virtual Seller Seller { get; set; }// и это (так связывается один ко многим)
+        public DateTime Created { get; set; }
+        public virtual ICollection<Sell> Sells { get; set; }
+        public override string ToString()
+        {
+            return $"№{CheckId} от {Created.ToString("dd.MM.yy hh:mm:ss")}";
+        }
+
+
+    }
+}
---
+++ CrmModelShop/CrmBl/Model/Seller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmBl.Model
+{
+    class Seller
+    {
+        public int SellerId { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Check> Checks { get; set; } // так связывается один ко многим
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
