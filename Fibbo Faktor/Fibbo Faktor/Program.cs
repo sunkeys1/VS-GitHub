93cfa59cc82596a8f0668c7ddf11182ebdbaec31
sunkeys1 <basatax5@gmail.com>
2021-09-21 20:08:29 +0700

    stack overflow

--- Fibbo Faktor/Fibbo Faktor/Program.cs
+++ Fibbo Faktor/Fibbo Faktor/Program.cs
@@ -10,7 +10,7 @@ namespace Fibbo_Faktor
     {
         static void Main(string[] args)
         {
-            for(int i = 0; i < 15; i++)
+            for (int i = 0; i < 15; i++)
             {
                 Console.Write(fibb(i) + " ");
             }
@@ -20,10 +20,24 @@ namespace Fibbo_Faktor
             Console.Write(fact(6));
             Console.WriteLine();
             Console.WriteLine(refact(5));
-
+            //int a = 5;
+            //while (true)
+            //{
+            //    rec(5);
+            //    Console.WriteLine(a++);
+            //}
 
             Console.ReadLine();
         }
+
+        //public static void rec(int n) для стек оверфлова
+        //{
+
+        //    Console.WriteLine(n++);
+
+        //    rec(n);
+
+        //}
         public static int fibb(int c)
         {
             int a = 0;
@@ -40,16 +54,16 @@ namespace Fibbo_Faktor
         public static int fact(int n)
         {
             int result = 1;
-            for(int i = 1; i <= n; i++)
+            for (int i = 1; i <= n; i++)
             {
                 result *= i; // result = result * i;
             }
             return result;
         }
-        public static int refact(int b) 
+        public static int refact(int b)
         {
             int result2 = 1;
-            if(b == 0)
+            if (b == 0)
             {
                 return 1;
             }
