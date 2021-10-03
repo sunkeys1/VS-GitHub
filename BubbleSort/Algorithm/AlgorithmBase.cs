876e3d6aa44c85bcd1243ea8795e68f39a6de817
sunkeys1 <basatax5@gmail.com>
2021-10-03 20:17:41 +0700

    bubble sort

---
+++ BubbleSort/Algorithm/AlgorithmBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    public class AlgorithmBase<T> where T : IComparable
+    {
+        public List<T> Items { get; set; } = new List<T>();
+        protected void Swap(int positionA, int positionB)
+        {
+            if(positionA < Items.Count && positionB < Items.Count)
+            {
+                var temp = Items[positionA];
+                Items[positionA] = Items[positionB];
+                Items[positionB] = temp;
+            }
+        }
+        
+        public virtual void Sort()
+        {
+            Items.Sort();
+        }
+    }
+    
+}
---
+++ BubbleSort/Algorithm/BbSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    public class BbSort<T> : AlgorithmBase<T> where T : IComparable
+    {
+        public override void Sort()
+        {
+            var count = Items.Count;
+
+            for(int j = 0; j < count; j++)
+            {
+                for (int i = 0; i < count - 1; i++)
+                {
+                    var a = Items[i];
+                    var b = Items[i + 1];
+                    if (a.CompareTo(b) == 1) // компарабле для сравнения штук с T типом можно юзать
+                    {
+                        Swap(i, i + 1);
+                    }
+                }
+                count--;
+            }
+            
+
+        }
+    }
+}
